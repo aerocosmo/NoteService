ac6ec9b089f5c11727ea01717cfcb0bd1e1d2822
aerocosmo <you@example.com>
2025-04-30 16:26:27 +0300

    Search update. Regarless of the case

--- src/main/java/com/notes/repository/NoteRepository.java
+++ src/main/java/com/notes/repository/NoteRepository.java
@@ -11,11 +11,20 @@ import java.util.List; // Добавьте этот импорт
 public interface NoteRepository extends JpaRepository<Note, Long> {
     // Базовые CRUD операции уже включены в JpaRepository
 
-    @Query("SELECT n FROM Note n WHERE " +
-       "LOWER(n.title) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
-       "LOWER(n.content) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
-       "LOWER(n.tags) LIKE LOWER(CONCAT('%', :query, '%'))")
-	List<Note> searchNotes(@Param("query") String query);
+//    @Query("SELECT n FROM Note n WHERE " +
+//       "LOWER(n.title) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+//       "LOWER(n.content) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+//       "LOWER(n.tags) LIKE LOWER(CONCAT('%', :query, '%'))")
+//	List<Note> searchNotes(@Param("query") String query);
+	
+      @Query("SELECT n FROM Note n WHERE " +
+     	// Преобразуем и поле title, и поисковый запрос в нижний регистр перед сравнением
+      	"LOWER(n.title) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+       	// Аналогично для поля content
+       	"LOWER(n.content) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+       	// И для поля tags
+       	"LOWER(n.tags) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Note> searchNotes(@Param("query") String query);
     // Метод для поиска заметок по id категории
     List<Note> findByTitleContainingOrContentContainingIgnoreCase(String title, String content);
 }
